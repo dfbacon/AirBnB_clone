e03d752fe93b7c758a3e2b6673c2b53b02e205cc
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-03-15 18:57:23 +0000

    add sudo command to everything

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,35 +5,35 @@ sudo apt-get install -y nginx
 
 # check and create /data directory
 if [ ! -d /data ]; then
-    mkdir /data
+    sudo mkdir /data
 fi
 
 # check and create /data/web_static directory
 if [ ! -d /data/web_static ]; then
-    mkdir /data/web_static
+    sudo mkdir /data/web_static
 fi
 
 # check and create /data/web_static/releases
 if [ ! -d /data/web_static/releases ]; then
-    mkdir /data/web_static/releases
+    sudo mkdir /data/web_static/releases
 fi
 
 # check and create /data/web_static/shared
 if [ ! -d /data/web_static/shared ]; then
-    mkdir /data/web_static/shared
+    sudo mkdir /data/web_static/shared
 fi
 
 # check and create /data/web_static/releases/test
 if [ ! -d /data/web_static/releases/test ]; then
-    mkdir /data/web_static/releases/test
+    sudo mkdir /data/web_static/releases/test
 fi
 
 # check and create index.html file
 if [ ! -f /data/web_static/releases/test/index.html ]; then
-    echo "Testing" | sudo tee /data/web_static/releases/test/index.html
+    echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
 fi
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data
 
