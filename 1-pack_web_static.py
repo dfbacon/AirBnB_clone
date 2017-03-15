e228edc47779535340d6d27972cf3bc8657de19e
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-03-15 21:52:30 +0000

    archive to tgz

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/user/bin/python3
+'''
+This is the '1-pack-web-static' module.
+
+1-pack-web-static is a Fabric script that generates a .tgz archive from a given
+location.
+
+This module contains 1 function: do_pack().
+'''
+
+
+def do_pack():
+    '''This is the 'do_pack' function.
+    do_pack generates a .tgz file and returns the archive path, or None if fail.
+    '''
+    local("sudo mkdir -p versions/")
+    local("sudo tar -zcvf \"./versions/web_static_`date +%Y%m%d%H%M%S`.tgz\"\
+    web_static")
