3b3cc993ee2fd3979f42ef06ca478489e9e12957
Daniel Bacon <daniel.bacon@holbertonschool.com>
2017-03-17 19:44:20 +0000

    re-writes for do_pack and do_deploy functions

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,23 +2,31 @@
 '''
 This is the '3-deploy_web_static' module.
 '''
-from fabric.api import *
+from fabric.api import run, put, env, local
+from fabric.decorators import runs_once
+from datetime import datetime
+import os
 
 env.hosts = ['34.207.77.115', '54.208.161.26']
 
 
+@runs_once
 def do_pack():
     '''This is the 'do_pack' function.
     do_pack generates a .tgz file and returns the archive path, or None.
     '''
+    local("sudo mkdir -p versions")
+    tar_1 = "tar czfv web_static_"
+    tar_2 = datetime.now().__format__("%Y%m%d%H%M%S")
+    tar_3 = ".tgz web_static"
+    local(tar_1 + tar_2 + tar_3)
+    archive_name = "web_static_" + tar_2 + ".tgz"
+    new_name = "./versions/" + archive_name
     try:
-        local("sudo mkdir -p versions/")
-        path = local(
-            "sudo tar -zcvf\"./versions/web_static_`date +%Y%m%d%H%M%S`.tgz\"\
-            web_static")
-        return(path)
+        local("sudo mv " + archive_name + " " + new_name)
     except:
         return(None)
+    return("./versions/" + archive_name)
 
 
 def do_deploy(archive_path):
@@ -27,43 +35,31 @@ def do_deploy(archive_path):
     The function distributes an archive to a web server, or returns False.
     '''
     try:
-        # upload archive to /tmp directory of web server
-        put(archive_path, "/tmp")
-        path = archive_path.strip("versions/")
-        s1 = "sudo mkdir -p /data/web_static/releases/" + path
-        run(s1)
-
-        # uncompress archive
-        s2 = "sudo tar -xzf /tmp/" + path + " -C /data/web_static/releases/"
-        s2 += path
-        run(s2)
-
-        # delete archive from web server
-        run("rm /tmp/" + path)
-
-        # delete symlink /data/web_server/current from web server
-        s3 = "sudo mv /data/web_static/releases/" + path
-        s3 += "/web_static/* /data/web_static/releases/" + path + "/"
-        run(s3)
-        s4 = "sudo rm -rf /data/web_static/releases/" + path + "/web_static"
-        run(s4)
+        put(archive_path, "/tmp/")
+        a, b = archive_path.find("web_static_"), archive_path.find(".tgz")
+        filename = archive_path[a:b]
+        make_dir = "sudo mkdir -p /data/web_static/releases/" + filename
+        run(make_dir)
+        tar = "sudo tar -xzf /tmp/" + filename + ".tgz -C "
+        tar = tar + "/data/web_static/releases/" + filename + "/"
+        run(tar)
+        run("sudo rm /tmp/" + filename + ".tgz")
         run("sudo rm -rf /data/web_static/current")
-
-        # create new symlink /data/web_server/current to new version of code
-        s5 = "sudo ln -s /data/web_static/releases/" + path
-        s5 += "/ /data/web_static/current"
-        run(s5)
+        name_a = "sudo mv /data/web_static/releases/" + filename
+        name_b = "/web_static/* /data/web_static/releases/" + filename + "/"
+        run(name_a + name_b)
+        link_a = "sudo ln -s /data/web_static/releases/" + filename + "/"
+        link_b = " /data/web_static/current"
+        run(link_a + link_b)
         return(True)
-
-    except Exception as e:
-        print(e)
+    except:
         return(False)
 
 
 def deploy():
     '''This is the deploy function.
     '''
-    path = do_pack()
-    if not path:
+    archive_path = do_pack()
+    if archive_path is None:
         return(False)
-    return(do_deploy(path))
+    return(do_deploy(archive_path))
